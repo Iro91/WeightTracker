055af567bbf23a6b32ef49510cff0fb231c22da4
Iro91 <ivromero19@gmail.com>
2017-04-23 14:46:13 -0700

    Added activity to log details entered into tthe edit box.

--- src/main/java/com/dataservice/woolly/weighttracker/WeightTracker.java
+++ src/main/java/com/dataservice/woolly/weighttracker/WeightTracker.java
@@ -1,13 +1,29 @@
 package com.dataservice.woolly.weighttracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class WeightTracker extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight_tracker);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
