ffeee97a98bd54ae9c7f007e86e10913072301e9
Iro91 <ivromero19@gmail.com>
2017-04-23 15:02:11 -0700

    Added text box to display information set by the user

--- src/main/java/com/dataservice/woolly/weighttracker/DisplayMessageActivity.java
+++ src/main/java/com/dataservice/woolly/weighttracker/DisplayMessageActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import java.util.LinkedList;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    /* Linked list used to store the dates acquired by the send button.  */
+    LinkedList<String> weightsList = new LinkedList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +19,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(WeightTracker.EXTRA_MESSAGE);
+        weightsList.add(message);
 
         // Capture the layout's TextView and set the string as its text
+        String displayText = "";
+        for(String data : weightsList){
+            displayText += data + "\n";
+        }
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(message);
+        textView.setText(displayText);
     }
 }
--- src/main/java/com/dataservice/woolly/weighttracker/WeightTracker.java
+++ src/main/java/com/dataservice/woolly/weighttracker/WeightTracker.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import java.util.LinkedList;
 
 public class WeightTracker extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.message";
+
+    /* Linked list used to store the dates acquired by the send button.  */
+    LinkedList<String> weightsList = new LinkedList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,21 @@ public class WeightTracker extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+
+
+
+        // Capture the layout's TextView and set the string as its text
+        weightsList.add(message);
+        String displayText = "";
+        for(String data : weightsList){
+            displayText += data + "\n";
+        }
+        TextView textView = (TextView) findViewById(R.id.CollectedValues);
+        textView.setText(displayText);
     }
 }
